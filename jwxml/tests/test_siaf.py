dfffe275d95b407f5ba68f2c853204b2596ee7d1
Joseph Long <jlong@stsci.edu>
2016-09-06 18:37:01 -0400

    Complete SIAF Aperture test conversion to py.test

--- jwxml/tests/test_siaf.py
+++ jwxml/tests/test_siaf.py
@@ -10,6 +10,8 @@ TEST_PRD_VERSION = 'PRDDEVSOC-D-012'  # updated 2016-04-13
 TEST_DATA_ROOT = os.path.join(DATA_ROOT, TEST_PRD_VERSION)
 
 def pair_almost_equal(left, right, atol=1e-6):
+    """Compare whether two 2-tuples (pairs) of numeric values have an
+    absolute difference of < `atol`"""
     left_1, left_2 = left
     right_1, right_2 = right
     assert abs(left_1 - right_1) < atol, "first elements differ by more than {}".format(atol)
@@ -21,26 +23,31 @@ def nircam_siaf():
     return SIAF(instr='NIRCam', basepath=TEST_DATA_ROOT)
 
 def test_transform_in_to_out(nircam_siaf):
-    startx = 1023
-    starty = 1024
-    nca = nircam_siaf['NIRCAM A']
+    nca = nircam_siaf['NRCA4_FULL']
+    x_det_ref, y_det_ref = nca.XDetRef, nca.YDetRef
 
-    assert pair_almost_equal(nca.Det2Sci(startx, starty), (1020., 1020.))
-    assert pair_almost_equal(nca.Det2Idl(startx, starty), (0.0, 0.0))
-    assert pair_almost_equal(nca.Det2Tel(startx, starty), (87.50, -497.10))
+    # Test fixed points
+    # -- det and sci ref points are == on NRC
+    assert pair_almost_equal(nca.Det2Sci(x_det_ref, y_det_ref), (x_det_ref, y_det_ref))
+    # -- {X,Y}DetRef is the Idl frame 0, 0 point
+    assert pair_almost_equal(nca.Det2Idl(x_det_ref, y_det_ref), (0.0, 0.0))
+    # -- The Tel frame ref point is {V2,V3}Ref
+    assert pair_almost_equal(nca.Det2Tel(x_det_ref, y_det_ref), (nca.V2Ref, nca.V3Ref))
 
 def test_transform_out_to_in(nircam_siaf):
-    startV2 = 87.50
-    startV3 = -497.10
-    nca = nircam_siaf['NIRCAM A']
+    nca = nircam_siaf['NRCA4_FULL']
+    v2_ref, v3_ref = nca.V2Ref, nca.V3Ref
 
-    assert pair_almost_equal(nca.Sci2Det(1020., 1020), (1023., 1024.))
-    assert pair_almost_equal(nca.Tel2Idl(startV2, startV3), (0.0, 0.0))
-    assert pair_almost_equal(nca.Tel2Sci(startV2, startV3), (1020., 1020.))
-    assert pair_almost_equal(nca.Tel2Det(startV2, startV3), (1023., 1024.))
+    # Test fixed points
+    # -- {X,Y}DetRef is the Idl frame 0, 0 point
+    assert pair_almost_equal(nca.Tel2Idl(v2_ref, v3_ref), (0.0, 0.0))
+    # -- det and sci ref points are == on NRC
+    assert pair_almost_equal(nca.Tel2Sci(v2_ref, v3_ref), (nca.XDetRef, nca.YDetRef))
+    # -- The Tel frame ref point is {V2,V3}Ref
+    assert pair_almost_equal(nca.Tel2Det(v2_ref, v3_ref), (nca.XDetRef, nca.YDetRef))
 
 def test_inverse_transforms(nircam_siaf):
-    nca = nircam_siaf['NIRCAM A']
+    nca = nircam_siaf['NRCA4_FULL']
 
     assert pair_almost_equal(nca.Det2Sci(*nca.Sci2Det(1020., 1020)), (1020., 1020))
     assert pair_almost_equal(nca.Sci2Det(*nca.Det2Sci(1020., 1020)), (1020., 1020))
